07e23c747567f6ef71e1711bcaf74177a74110f6
Dmitry <dimajanuary@gmail.com>
2025-08-27 00:04:07 +0300

    init

---
+++ src/test/java/it/luncent/cloud_storage/TestCloudStorageApplication.java
@@ -0,0 +1,11 @@
+package it.luncent.cloud_storage;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCloudStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CloudStorageApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
---
+++ src/test/java/it/luncent/cloud_storage/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package it.luncent.cloud_storage;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
